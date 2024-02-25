2639fda31fa3300c2cb394c32a7df14f277580b3
name <aziz.abdusamiev@gmail.com>
2024-02-25 16:28:42 -0500

    got the logic of point right

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ sentences = open('my_sentences.json','r')
 
 data = json.load(sentences)
 len_data = len(data['sentences'])
-print(len_data)
+# print(len_data)
 # print(type(data))
 
 class Player:
@@ -25,22 +25,30 @@ class Player:
         return f"Player: {self.name} with {self.score} points"
 
 def shuffle(x):
+    d = {}
+    for i in range(len(x)):
+        d[i] = i
     for i in range(1000):
-        p1, p2 = random.randint(0,len(pool)-1),random.randint(0,len(pool)-1)
+        p1, p2 = random.randint(0,len(x)-1),random.randint(0,len(x)-1)
         x[p1], x[p2] = x[p2], x[p1]
-        
-all_players = []
+        s = d[p1]
+        d[p1] = d[p2]
+        d[p2] = s
+    return d
+
+all_players = [Player('game')]
 
 num_players = 2 # int(input("Enter the amount of players: "))
 for i in range(num_players):
-    name = "Gabe" # input("Enter you nick name: ")
+    name = f"P{i+1}" # input("Enter you nick name: ")
     player = Player(name)
     all_players.append(player)
 
 turn = 1
 appeared = set()
+num_players = len(all_players)
 
-while turn < 2:
+while turn < 3:
     turn += 1
     pool = []
     ran_num = random.randint(0, len(data['sentences'])-1)
@@ -50,30 +58,31 @@ while turn < 2:
     entity = data['sentences'][ran_num]
     pool.append(entity['english'])
     print(f"The sentence is:\n{entity['spanish']}\n{entity['spanish_pronunciation']}")
-    for i in range(num_players):
+    for i in range(1,num_players):
         # timer
-        guess = input('Enter you aproximation: ')
+        guess = input(f'{all_players[i].name} enter you aproximation: ')
         all_players[i].guess = guess
         pool.append(guess)
     ordered_pool = copy.copy(pool)
-    shuffle(pool)
-    print(ordered_pool)
-    print(f'Please select the right answer from the pool of the answers:')
+    ds = shuffle(pool)
+    # print(pool)
+    # print(ds)
+    # print(f'Please select the right answer from the pool of the answers:')
     for i in range(len(pool)):
         print(f"{i+1}) {pool[i]}")
-    for i in range(num_players):
+    for i in range(1, num_players):
         choice = int(input(f"{all_players[i].name}: "))
-        if choice-1 == 0:
+        if ds[choice-1] == 0:
             all_players[i].score += 2
-        elif choice-1 == i:
+        elif ds[choice-1] == i:
             pass
-        elif choice > 0 or choice < num_players:
-            all_players[choice-1].score += 1
-            all_players[choice-1].fooled.append(all_players[i])
+        elif choice > 0 and choice <= num_players+1:
+            all_players[ds[choice-1]].score += 1
+            all_players[ds[choice-1]].fooled.append(all_players[i])
         else:
             pass
-    for i in range(num_players):
-        print(all_players[i].score)
+    for i in range(1, num_players):
+        print(all_players[i])
 
     
     
---
+++ shuffle_mapping.py
@@ -0,0 +1,18 @@
+import random
+
+def shuffle(x):
+    d = {}
+    for i in range(len(x)):
+        d[i] = i
+    for i in range(1000):
+        p1, p2 = random.randint(0,len(x)-1),random.randint(0,len(x)-1)
+        x[p1], x[p2] = x[p2], x[p1]
+        s = d[p1]
+        d[p1] = d[p2]
+        d[p2] = s
+    return d
+
+l = list(range(10))
+print(l)
+shuffle(l)
+print(l)
